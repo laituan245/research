c5a20b700ab9a11d72dfe0ccd5bd279d8312be9a
laituan245 <laituan245@kaist.ac.kr>
2016-02-12 18:10:28 +0900

    Updated analyze_nodes_in_pegonet.py

--- analyze_nodes_in_pegonet.py
+++ analyze_nodes_in_pegonet.py
@@ -10,6 +10,9 @@ nbOfNodes = 0
 property_map_first = {}
 property_map_second = {}
 property_map_third = {}
+existAt1 = {}
+existAt2 = {}
+existAt3 = {}
 in_pegonet_count = {}
 
 choice = int(raw_input("Input your choice (1~3):"))
@@ -29,6 +32,16 @@ for line in f:
         property_map_first[nodeid] = [int(tmpvar) for tmpvar in tmplist[1].split('/')]
         property_map_second[nodeid] = [int(tmpvar) for tmpvar in tmplist[2].split('/')]
         property_map_third[nodeid] = [int(tmpvar) for tmpvar in tmplist[3].split('/')]
+
+        existAt1[nodeid] = True
+        existAt2[nodeid] = True
+        existAt3[nodeid] = True
+        if sum(property_map_first[nodeid]) == 0:
+            existAt1[nodeid] = False
+        if sum(property_map_second[nodeid]) == 0:
+            existAt2[nodeid] = False
+        if sum(property_map_third[nodeid]) == 0:
+            existAt3[nodeid] = False
         in_pegonet_count[nodeid] = 0
 
 print("Read the info of all nodes")
@@ -68,19 +81,28 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 newdataset = []
 for curnodeid in range(nbOfNodes):
-    nodesize = property_map_second[curnodeid][0]
+    nodeproperties = property_map_second[curnodeid]
     if choice < 3:
-        nodesize = property_map_first[curnodeid][0]
-    newdataset.append((curnodeid, in_pegonet_count[curnodeid],nodesize))
+        nodeproperties = property_map_first[curnodeid]
+
+    newdataset.append((curnodeid, in_pegonet_count[curnodeid],nodeproperties[0], nodeproperties[1], nodeproperties[2], nodeproperties[3], nodeproperties[4]))
 print("Finished the analysis")
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
 with open('csv_output/'+csvoutputfilename, 'w') as csvfile:
-    fieldnames = ['nodeid', '#Pegonets', 'nodesize']
+    fieldnames = ['nodeid', '#Pegonets', 'nodesize', '#instances', '#instancesHavingType', '#instancesRedirected', 'infoboxLength']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for data in newdataset:
-        writer.writerow({'nodeid': data[0], '#Pegonets': data[1], 'nodesize': data[2]})
+        current_node_id = int(data[0])
+        if ((choice < 3 and existAt1[current_node_id]) or (choice == 3 and existAt2[current_node_id])):
+            writer.writerow({'nodeid': data[0],
+                            '#Pegonets': data[1],
+                            'nodesize': data[2],
+                            '#instances': data[3],
+                            '#instancesHavingType': data[4],
+                            '#instancesRedirected': data[5],
+                            'infoboxLength': data[6]})
 print("Write the analysis result to a csv file")
 print("--- %s seconds ---" % (time.time() - start_time))
